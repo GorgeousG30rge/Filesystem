f43642dde8d638d5e1bdc7bcdfb3851791f6b6a8
schoolbolt <info@schoolbolt.ru>
2020-03-10 20:27:13 +0500

    Classes changed, added new methods

--- Filesystem.py
+++ Filesystem.py
@@ -7,7 +7,12 @@ class MyFile(MyFileSystemObject):
     def __init__(self, name, date):
         MyFileSystemObject.__init__(self, name)
         self.creation_date = date
+        self.content = '' #хранит текст
+    
 
+    def show_contents(self, i):
+        print('\t' * i, self.name)
+        
 
 
 class MyFolder(MyFileSystemObject):
@@ -18,8 +23,20 @@ class MyFolder(MyFileSystemObject):
     def add_element(self, element):
         self.contents.append(element)
 
+    def remove_element(self, element):
+        self.contents.remove(element)
+
+    def show_contents(self, i):
+        print('\t' * i, self.name + '/')
+        for element in self.contents:
+            element.show_contents(i + 1)
+        print('\t')
 
 
+""" Метод clone:
+для каждого класса;
+копирует свойства
+и возвращает новый объект"""
         
 
 
