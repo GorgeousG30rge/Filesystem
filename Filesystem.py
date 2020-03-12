597ecce6b45bec8c7a1b3a96bab6db1e790319de
schoolbolt <info@schoolbolt.ru>
2020-03-12 20:27:17 +0500

    added Caretaker, modified clone methods

--- Filesystem.py
+++ Filesystem.py
@@ -1,27 +1,29 @@
 class MyFileSystemObject:
-    def __init__(self, name):
+    def __init__(self, name, date):
         self.name = name
+        self.creation_date = date
 
 
 class MyFile(MyFileSystemObject):
     def __init__(self, name, date):
-        MyFileSystemObject.__init__(self, name)
-        self.creation_date = date
+        MyFileSystemObject.__init__(self, name, date)
         self.content = '' #хранит текст
     
 
     def show_contents(self, i):
         print('\t' * i, self.name)
 
-    def clone(self, obj):
-        clone = obj
+    def clone(self):
+        """ Метод clone: для каждого класса; копирует свойства и возвращает новый объект"""
+        clone = MyFile(self.name, self.creation_date)
+        clone.content = self.content
         return clone
         
 
 
 class MyFolder(MyFileSystemObject):
-    def __init__(self, name):
-        MyFileSystemObject.__init__(self, name)
+    def __init__(self, name, date):
+        MyFileSystemObject.__init__(self, name, date)
         self.contents= []
 
     def add_element(self, element):
@@ -36,15 +38,33 @@ class MyFolder(MyFileSystemObject):
             element.show_contents(i + 1)
         print('\t')
 
-    def clone(self, obj):
-        clone = obj
+    def clone(self):
+        """ Метод clone: для каждого класса; копирует свойства и возвращает новый объект"""
+        clone = MyFolder(self.name, self.creation_date)
+        for element in self.contents:
+            clone.contents.append(element.clone())
+
         return clone
 
 
-""" Метод clone:
-для каждого класса;
-копирует свойства
-и возвращает новый объект"""
+
+class Caretaker:
+    def __init__(self):
+        self.storage = []
+
+    def show_storage(self):
+        for i, copy in enumerate(self.storage):
+            print(i, copy)
+
+    def restore_copy(self, index):
+        return self.storage[index]
+
+    def store_copy(self, copy):
+        self.storage.append(copy)
+        
+
+
+
         
 
 
